d170a4d02141a28110df428857515993983b6145
JesusMunnoz <jesusmunozpost@gmail.com>
2024-04-04 18:36:24 +0200

    atasco en intro de actors, director y writer

--- movie.ts
+++ movie.ts
@@ -20,40 +20,48 @@ export class Movie{
     constructor (title:string, releaseYear: number, nationality: string, genre: string){
         this.title = title;
         this.releaseYear = releaseYear;
-        this.actors = []
+        this.actors;
         this.nationality = nationality;
-        this.director = Professional[pf2.infoProf()];
-        this.writer = pf4;
-        this.language = "English";
-        this.platform = "Netflix";
-        this.isMCU = false;
-        this.mainCharacterName = "Margot Robbie, Leonardo DiCaprio and Brad Pitt.";
-        this.producer = "Quentin Tarantino";
-        this.distributor = "Sony Pictures";
+        this.director;
+        this.writer;
+        this.language ;
+        this.platform;
+        this.isMCU ;
+        this.mainCharacterName;
+        this.producer ;
+        this.distributor ;
         this.genre = genre;
     }
 
     //Metodos
-    public filmActors(actors : Professional):void {
-        console.log(this.actors.push(actors));
-    }
+   /* public filmActors(Professional) {
+        return this.actors.push(Professional);
+    }*/
 
     public infoMv(): string{
         return `Title: ${this.title}\nRelease year: ${this.releaseYear}\nActors: ${this.actors}\nNationality: ${this.nationality}\nDirector: ${this.director}\nWriter: ${this.writer}\nLanguage: ${this.language}\nPlatform: ${this.platform}\nIs a MCU: ${this.isMCU}\nMain Character: ${this.mainCharacterName}\nProducer: ${this.producer}\nDistrubutor: ${this.distributor}\nGenre: ${this.genre}\n`
     }
 }
 
-let pf1: Professional = new Professional ("Quentin Tarantino", 61, 85, 1.80, false, "United States", 2, "Actor");
-let pf2: Professional = new Professional ("Quentin Tarantino", 61, 85, 1.80, false, "United States", 2, "Director");
-let pf3: Professional = new Professional ("Quentin Tarantino", 61, 85, 1.80, false, "United States", 2, "Producer");
-let pf4: Professional = new Professional ("Quentin Tarantino", 61, 85, 1.80, false, "United States", 2, "Writer");
-let pf5: Professional = new Professional ("Brad Pitt", 61, 80, 1.80, false, "United States", 2, "Actor");
-let pf6: Professional = new Professional ("Leonardo DiCaprio", 49, 85, 1.70, false, "United States", 1, "Actor");
-let pf7: Professional = new Professional ("Margot Robbie", 33, 80, 1.83, false, "Australia", 0, "Actor");
+let pf1: Professional = new Professional ("Quentin Tarantino", 61, 85, 1.80, false, "United States", 2, "Actor/Director/Producer/Writer");
+let pf2: Professional = new Professional ("Brad Pitt", 61, 80, 1.80, false, "United States", 2, "Actor");
+let pf3: Professional = new Professional ("Leonardo DiCaprio", 49, 85, 1.70, false, "United States", 1, "Actor");
+let pf4: Professional = new Professional ("Margot Robbie", 33, 80, 1.83, false, "Australia", 0, "Actor");
 
-let flmActrs : Movie = new Movie();
+
 
 let mv1 : Movie = new Movie ("Once Upon a Time in Hollywood", 2019, "United States", "Comedy/Drama");
+
+//console.log(mv1.filmActors(pf1));
+
+mv1.actors = [pf1];
+mv1.director = pf1;
+mv1.writer = (pf1);
+mv1.language = "English";
+mv1.platform = "Netflix";
+mv1.isMCU = false;
+mv1.mainCharacterName = "Margot, Leo and Brad";
+mv1.producer = "Quentin Tarantino";
+mv1.distributor = "Sony Pictures"
+
 console.log(mv1.infoMv());
-
-
